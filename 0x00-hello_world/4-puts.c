e22dc7ecbc6dc964932e42fe37856a8af0dcafbd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-05 15:16:43 -0700

    first c program added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This contains the 'put' function
+ * Return: 0 if execution is successfull
+ */
+
+int main(void)
+{        
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
