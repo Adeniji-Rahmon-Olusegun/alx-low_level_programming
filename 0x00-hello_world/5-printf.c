0974e8546775811fcb5c0e9dcdb2384e86c999b6
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-05 15:44:04 -0700

    first c program added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a one line using the ptrinf statement
+ * Return: 0 if execution is successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
