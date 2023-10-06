49a270634494ad534d981e26f73a16a06f56db66
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-06 16:06:20 -0700

    README.md file added and 0-positive_or_negative.c file added

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - genrates a random number and prints same
+ *  Return: 0 if executed successfully
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	} else if (n == 0)
+	{
+		printf("is zero\n");
+	} else if (n < 0)
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains C programs that uses the if_else_while statements.
