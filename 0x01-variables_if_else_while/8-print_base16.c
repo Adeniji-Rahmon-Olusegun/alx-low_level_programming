8b45467b3252defd7f36fb3dd9fe3ebb090c6a15
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 15:05:02 -0700

    8-print_base16.c file added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints lower and upper case alpha
+ * Return: 0 if successfully exec.
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int lim = 10;
+	char lower_alpha[] = "abcdef";
+	size_t alpha_len = strlen(lower_alpha);
+
+	for (i = 0; i <= lim - 1; i++)
+		putchar('0' + i);
+	for (j = 0; j <= (signed int) alpha_len - 1; j++)
+		putchar(upper_alpha[j]);
+	putchar('\n');
+	return (0);
+}
