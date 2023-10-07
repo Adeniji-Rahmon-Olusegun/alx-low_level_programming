ff04ce9982076cc28944cc2143262b990100cb13
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 14:27:54 -0700

    6-print_numberz.c file added

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,9 @@
 int main(void)
 {
 	int i;
-	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int len_arr = sizeof(arr) / sizeof(arr[0]);
 
-	for (i = 0; i <= len_arr - 1; i++)
-		putchar(arr[i]);
+	for (i = 0; i < 10; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
