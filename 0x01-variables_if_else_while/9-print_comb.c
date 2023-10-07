32b1bb6dfbc01230568e04d03bbbfe2c80ade114
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 15:13:05 -0700

    9-print_comb.c file added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all the digits
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int len_arr = sizeof(arr) / sizeof(arr[0]);
+
+	for (i = 0; i <= len_arr - 1; i++)
+	{
+		putchar('0' + arr[i]);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
