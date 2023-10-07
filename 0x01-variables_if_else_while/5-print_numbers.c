da8fa1cca0b246f09c45fc916006e8c48219903b
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 14:08:02 -0700

    5-print_numbers.c file added

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,10 +12,10 @@ int main(void)
 	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int len_arr = sizeof(arr) / sizeof(arr[0]);
 
-	for (i = 0; i < len_arr - 1; i++)
+	for (i = 0; i <= len_arr - 1; i++)
 	{
 		printf("%d", arr[i]);
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
