79c33680ffd5fb55fbf80a35d9110f07d4514ea5
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 12:11:08 -0700

    3-print_alphabets.c file added

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int i;
+	int j;
 	char lower_alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	char upper_alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	size_t alpha_len = strlen(lower_alpha);
 
 	for (i = 0; i <= (signed int) alpha_len - 1; i++)
-	{
 		putchar(lower_alpha[i]);
-		putchar(upper_alpha[i]);
-	}
+	for (j = 0; j <= (signed int) alpha_len - 1; j++)
+		putchar(upper_alpha[j]);
 	putchar('\n');
 	return (0);
 }
