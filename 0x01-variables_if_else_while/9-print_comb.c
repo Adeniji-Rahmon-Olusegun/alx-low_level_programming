73121bb372a16ea029bfdecad028beb48500f3cd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 15:47:16 -0700

    9-print_comb.c file added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@
 int main(void)
 {
 	int i;
-	int j = 1;
+	int j;
 	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int len_arr = sizeof(arr) / sizeof(arr[0]);
 
 	for (i = 0; i <= len_arr - 1; i++)
 	{
 		putchar('0' + arr[i]);
+
+		j = 1;
 		while (j <= 1)
 		{
 			putchar(',');
