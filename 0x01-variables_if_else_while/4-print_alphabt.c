f1089d8164b5426e0626158366841b4aabd0d161
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 12:42:49 -0700

    4-print_alphabt.c file added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets except q and e
+ * Return: 0 if executed successfully
+ */
+
+int main(void)
+{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	size_t str_len = strlen(alpha);
+
+	for (i = 0; i <= (signed int) str_len - 1; i++)
+	{
+		if (alpha[i] != 'q' || alpha[i] != 'e')
+		{
+			putchar(alpha[i]);
+		} else
+		{
+			{}
+		}
+	putchar('\n');
+	return (0);
+	}
+}
