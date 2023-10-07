dd3057dd8a488472e44005f3f2c4ed2f120954a1
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 14:47:52 -0700

    7-print_tebahpla.c file added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabets in lower case
+ * Return: 0 if executed successfully
+ */
+
+int main(void)
+{
+	int i;
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	size_t alpha_len = strlen(alphabets);
+
+	for (i = (signed int) alpha_len - 1; i = 0;  i--)
+		putchar(alphabets[i]);
+	putchar('\n');
+	return (0);
+}
