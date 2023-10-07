67bbf91326a0ca0619afa9e46cde441a329f6907
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-07 14:20:27 -0700

    6-print_numberz.c file added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints digits
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int len_arr = sizeof(arr) / sizeof(arr[0]);
+
+	for (i = 0; i <= len_arr - 1; i++)
+		putchar(arr[i]);
+	
+	putchar('\n');
+	
+	return (0);
+}
