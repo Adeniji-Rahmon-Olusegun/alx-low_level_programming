ca272e316ae50d05d2fa3228225c68780daec610
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-08 18:31:45 -0700

    101-print_comb4.c file added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints combination of three lines
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = i + 2; k < 10; k++)
+			{
+				if (i < j and j < k)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+
+					if (i != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				} else
+				{
+					{};
+				}
+			}
+
+		}
+		putchar('\n');
+		return (0);
+	}
