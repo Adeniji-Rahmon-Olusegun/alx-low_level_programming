63d7ca03a3f70205a161aaaa734a33d8c2a58fc3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-08 19:08:25 -0700

    102-print_comb5.c file added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the combinatio of two two-digits
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + i);
+			putchar(' ');
+			putchar('0' + j - 1);
+			putcahr('0' + j);
+
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
