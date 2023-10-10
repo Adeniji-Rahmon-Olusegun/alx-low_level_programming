a4e64f8954909fd434a30419a5f66333fcd456ec
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 15:55:43 -0700

    8-24_hours.c file added

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,21 +14,19 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			if (i < 10 || j < 10)
+			if (i < 10)
 			{
 				_putchar('0' + 0);
-				_putchar('0' + i);
-				_putchar(':');
-				_putchar('0' + 0);
-				_putchar('0' + j);
-				_putchar('\n');
-			} else
-			{
-				_putchar('0' + i);
-				_putchar(':');
-				_putchar('0' + j);
 			}
+			_putchar('0' + i);
+			_putchar(':');
 
+			if (j < 10)
+			{
+				_putchar('0' + 0);
+			}
+			_putchar('0' + j);
+			_putchar('\n');
 		}
 	}
 }
