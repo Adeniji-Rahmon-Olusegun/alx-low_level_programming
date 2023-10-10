818cccab592a103341162cb9dca087456fbd5f30
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 15:05:35 -0700

    7-print_last_digit.c file added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,14 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0 && n == -n)
+	int m;
+
+	m = n % 10;
+
+	if (n < 0)
 	{
-		_putchar('0' + (n % 10));
-		return (n % 10);
-	} else if (n < 0)
-	{
-		_putchar('0' + (-n % 10));
-		return (-n % 10);
+		m = -(m);
 	}
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	_putchar('0' + m);
+	return (m);
 }
