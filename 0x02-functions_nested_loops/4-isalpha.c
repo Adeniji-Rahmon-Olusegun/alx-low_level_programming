3abb92df282906499625f7d07fe2d5d0c67d8ff8
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 12:37:43 -0700

    4-isalpha.c file added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is an alphabet
+ * @c: character to be checked
+ * Return: 1 if alphabet and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((int)(c) != c)
+	{
+		if (c >= (int)('a') && c <= (int)('z'))
+		{
+			return (1);
+
+		} else if (c >= (int)('A') && c <= (int)('Z'))
+		{
+			return (1);
+		}
+
+	}
+	return (0);
+}
