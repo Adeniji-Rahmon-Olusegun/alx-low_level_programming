8f34bd29d38af64b2f22526732c5ac3c1ac46ae7
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 13:42:57 -0700

    5-sign.c file added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: absolute value of the integer
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	} else if (n == 0);
+	{
+		return (0);
+	}
+	return (-(n));
+}
+
