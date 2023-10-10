de3054b42934d0cf2fca34c7fae4d1fe737b25d8
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 08:11:03 -0700

    0-putchar.c file added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * _putchar - prints character to stdout
+ * @character: caharacter to print
  * Return: character to print
  */
-int _putchar(char character)
+void _putchar(char character)
 {
 	return (write(1, &character, 1));
 }
