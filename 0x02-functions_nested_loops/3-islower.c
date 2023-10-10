c0d163f8c5b42e1c3f32dd017f368503f7315217
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 12:08:44 -0700

    3-islower.c file added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a letter is lowercase
+ * @c: letter to check
+ * Return: 1 if c lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= (int)('a') && c <= (int)('z'))
+	{
+		return (1);	
+	}else
+	{
+		return (0);
+	}
+}
