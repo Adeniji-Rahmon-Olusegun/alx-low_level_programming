9d0d37354b1cb4f68aa110cb72ea6a43a575af12
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 10:36:31 -0700

    2-print_alphabet_x10.c file added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets x10
+ * @void: takes not argument
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+	int num_of_prints;
+	int len_alphabets;
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < num_of_prints; i++)
+	{
+		for (j = 0; j < len_alphabets; j++)
+		{
+			_putchar(alphabets[j]);
+		}
+	}
+	_putchar('\n');
+}
