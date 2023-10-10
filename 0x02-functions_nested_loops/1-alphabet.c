f62a141b8848eea4ab6d6ffffc8e14a0e621d495
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 09:55:10 -0700

    1-alphabet.c file added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
-#include "main.h"
+#include <unistd.h>
 /**
- * main - prints all alphabets in lowercase
- * Return: 0 if success
+ * print_alphabet - prints all the alphabets
+ * @void: receives no argument
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+        int i;
+        char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+        for (i = 0; i < 26; i++)
+        {
+                _putchar(alphabets[i]);
+        }
+
+        _putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints all alphabets in lowercase
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
