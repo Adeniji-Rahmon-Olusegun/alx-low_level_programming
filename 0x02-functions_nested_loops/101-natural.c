568acb39aaa8472d2fa54cb07952aac71af4c243
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 21:39:51 -0700

    101-natural.c file added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints multiples of 3 and 5
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int n = 1024;
+	int result = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+		{
+			result = result + i;
+		} else if (i % 5 == 0)
+		{
+			result = result + i;
+		}
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
