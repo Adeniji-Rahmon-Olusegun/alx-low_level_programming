e40817a962911f13da987e6bd28ea8f7f4514952
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 18:04:38 -0700

    9-times_table.c file added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,15 @@
 void times_table(void)
 {
 	int i, j;
+	int result;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar('0' + (i * j));
+			result = i * j;
+
+			_putchar('0' + result);
 			if (j < 9)
 			{
 				_putchar(',');
