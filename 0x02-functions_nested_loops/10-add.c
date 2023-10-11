cd597d4e7bbbba88034fc95ddfd5773cc26f4c24
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 19:11:32 -0700

    10-add.c file added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number to add
+ * @b: second number to add
+ * Return: result of the added number
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
