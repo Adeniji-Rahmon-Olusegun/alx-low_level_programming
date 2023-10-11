f7ab039e8d05516f667c60b53b07add6a2b4cd19
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-10 20:24:53 -0700

    100-times_table.c file added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints n size times table
+ * @n: size of the times table
+ * Return: n-size timetable
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 15 && n < 0)
+	{
+		{};
+	} else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j == 0)
+				{
+					printf("%d, ", (i * j));
+				} else
+				{
+					printf("%2d", (i * j));
+					if (j < n)
+					{
+						printf(", ");
+					}
+				}
+			}
+			printf("\n");
+		}
+	}
+}
