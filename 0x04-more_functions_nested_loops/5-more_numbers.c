ae618a45fdc9e35c90efefcd0a8c21ced50e7c9b
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-12 12:24:45 -0700

    5-more_numbers.c file added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14 ten times
+ * @void: no argument
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar('0' + j);
+		}
+
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
