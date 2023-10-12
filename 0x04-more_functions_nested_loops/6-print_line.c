a56d9fa0dc3826803b34929b7b262a1b4dc9e05f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-12 13:25:05 -0700

    6-print_line.c file added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints line n times
+ * @n: number of dahes
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
