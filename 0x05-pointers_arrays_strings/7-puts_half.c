783999f43a5ac7a650f2b8ccbcb76a97cd158a1d
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-16 18:36:54 -0700

    7-puts_half.c file added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the characters
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+	int counter;
+
+	counter = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	if (counter % 2 == 0)
+	{
+		for (j = (counter / 2) - 1; j < counter; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else if (counter % 2 != 0)
+	{
+		for (k = ((counter - 1) / 2) + 1; k < counter; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+
+	_putchar('\n');
+}
