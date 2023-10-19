d4014c9ed9227b4cf401bbfa125a330dc1b84804
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-19 08:53:06 -0700

    1-strncat.c file added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatebates two strings with at
+ * most n-bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k;
+	int dest_len_tracker = 0;
+	int src_len_tracker = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest_len_tracker++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		src_len_tracker++;
+	}
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k + dest_len_tracker] = src[k];
+	}
+
+	return (dest);
+}
