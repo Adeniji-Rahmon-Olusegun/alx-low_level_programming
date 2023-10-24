0f83c34ffc7374b33a0de057dd46c7b839a1fcbd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-23 17:12:23 -0700

    4-strpbrk.c file added

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - finds first occurence of the substring
+ * in the main string
+ * @haystack: main string
+ * @needle: substring
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (
+			}
+		}
+	}
+
+}
