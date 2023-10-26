85c17181d69bdf23ec1193927e8c188d171702ae
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-26 15:27:04 -0700

    4-pow_recursion.c file added

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - computes the of a number
+ * raised to the power of another
+ * @x: base number
+ * @y: power
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(y - 1));
+}
