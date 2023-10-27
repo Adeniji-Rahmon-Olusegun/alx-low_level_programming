5d0be9bddf54f818cd34506bf95596240e305304
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-27 11:24:25 -0700

    3-mul.c file added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two number
+ * @argc: argument count
+ * @argv: vector
+ * Return: result and 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	
+	if (argc - 1 > 2 || argc - 1 < 2)
+	{
+		printf("Error");
+
+		return (1);
+	}
+
+	return (argv[1] * argv[2]);
+}
