0e4d53ff0b02e47e2accb8885af0de477af40fbc
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-27 11:39:27 -0700

    3-mul.c file added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two number
@@ -9,6 +10,13 @@
 
 int main(int argc, char *argv[])
 {
+	int result;
+
+	int arg1 = atoi(argv[1]);
+	int arg2 = atoi(argv[2]);
+
+	result = arg1 * arg2;
+
 	if (argc - 1 > 2 || argc - 1 < 2)
 	{
 		printf("Error");
@@ -16,5 +24,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	return (argv[1] * argv[2]);
+	printf("%d\n", result);
+
+	return (result);
 }
