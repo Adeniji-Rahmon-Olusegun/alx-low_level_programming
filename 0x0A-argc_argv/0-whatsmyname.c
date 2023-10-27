d8749440e523a253d0e6e1b0ba2abc32bff34737
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-27 09:56:13 -0700

    README, main.h, _putchar.c and 0-whatsmyname.c files added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of of the program
+ * @argc: array count
+ * @argv: vector containing strings
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains C program files that illustrates the power and benefits  of using argc and argv arguments in the main function of C.
