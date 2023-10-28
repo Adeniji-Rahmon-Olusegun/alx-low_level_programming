bcf23702280c1df95a9288848a53a4920699b49e
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-28 12:45:09 -0700

    create_static_lib.sh file added

--- 0x09-static_libraries/atoi.c
+++ 0x09-static_libraries/atoi.c
@@ -6,7 +6,7 @@
  * Return: 0 if success
  */
 
-int _atoi(char *s)
+int _atoi(char __attribute__((unused)) *s)
 {
 	return (0);
 }
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c *.c | ar -rsc liball.a *.o
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c | ar -rsc liball.a *.o
 ranlib liball.a
