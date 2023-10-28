025f3fe0bbdbc53d2ab670bb3986d368a5930ab4
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-27 19:02:24 -0700

    4-add.c file added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,39 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	int i, is_a_number;
-	size_t j;
+	int i;
+	size_t string_checker;
 
-	int result = 0;
+	int result;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
-	}
-	else if (argc >= 2)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			is_a_number = 1;
-			for (j = 0; j < strlen(argv[i]); j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					is_a_number = 0;
-					break;
-				}
-			}
 
-			if (!is_a_number)
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (string_checker = 0; string_checker < strlen(argv[i]); string_checker++)
+		{
+			if (!isdigit(argv[i][string_checker]))
 			{
 				printf("Error\n");
-				exit(1);
-			}
-			else
-			{
-				result += atoi(argv[i]);
+				return (1);
 			}
 		}
+
+		result += atoi(argv[i]);
 	}
 
 	printf("%d\n", result);
