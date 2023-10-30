c686a97b9c5ec2aef7c2cf0da231c48dd9a42659
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-30 11:37:32 -0700

    1-strdup.c file added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer that points to a new
+ * string in a new allocated memory.
+ * @str: string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr_strcpy;
+
+	int i, str_len_count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str_len_count++;
+	}
+
+	ptr_strcpy = (char *)malloc(sizeof(char) * sizeof(str_lencounter + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if ((size_t sizeof(char) * sizeof(str_lencounter + 1)) < (str_len_count + 1))
+	{
+		return (NULL);
+	}
+
+	while (*str != '\0')
+	{
+		*ptr_strcpy = *str;
+
+		ptr_strcpy++;
+		str++;
+	}
+
+	return (ptr_strcpy);
+}
