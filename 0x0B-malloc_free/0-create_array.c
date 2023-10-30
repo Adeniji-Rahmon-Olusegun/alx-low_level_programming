db21459f809b53c0eece24535955aff706bc9ad7
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-30 10:33:06 -0700

    README.md, main.h and 0-create_array.c files added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters
+ * @size: array size
+ * @c: character for initialization
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr_memblock;
+	unsigned int tracker = 0;
+
+	ptr_memblock = (char *)malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	while (tracker < size)
+	{
+		*(ptr_memblock + tracker) = c;
+
+		tracker++;
+	}
+
+	return (ptr_memblock);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains C program files that illustrates dynamic memory allocation
