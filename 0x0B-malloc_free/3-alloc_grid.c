76b5a5d69f9a5b785979c75932bb6600aa874ab8
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-31 15:02:59 -0700

    3-alloc_grid.c file added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	ptr_zeros = (int **)malloc(sizeof(int) * height);
+	ptr_zeros = (int **)malloc(sizeof(int *) * height);
 
 	if (ptr_zeros == NULL)
 	{
@@ -48,10 +48,7 @@ int **alloc_grid(int width, int height)
 			free(ptr_zeros);
 			return (NULL);
 		}
-	}
 
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			*(ptr_zeros[i] + j) = 0;
 	}
