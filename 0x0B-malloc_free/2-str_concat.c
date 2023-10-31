d173dd30aeef0d20ce85cca5d49425f7c8e655ba
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-10-31 06:30:14 -0700

    2-str_concat.c file added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -44,7 +44,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = 1; s2[j] != '\0'; j++)
 		*(ptr_concat_str + (s1_len + j)) = s2[j];
 
-	//ptr_concat_str[j] = '\0';
+	/*ptr_concat_str[j] = '\0';*/
 
 	return (ptr_concat_str);
 }
