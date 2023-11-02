0e0a5ec0a508c10cabf15f079269e4e831daabe5
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-02 15:01:27 -0700

    3-array_rangex.c file added

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * from MIN to MAX
+ *
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr_array;
+	int i, arr_counter, index;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		arr_counter++;
+	}
+
+	ptr_array = (int *)malloc(sizeof(int) * arr_counter);
+
+	if (ptr_array == NULL)
+	{
+		return (NULL);
+	}
+
+	index = 0;
+
+	for (i = min; i <= max; i++)
+	{
+		*(ptr_array + index) = i;
+
+		index++;
+	}
+
+	return (ptr_array);
+}
