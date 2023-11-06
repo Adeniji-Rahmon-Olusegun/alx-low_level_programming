1876e7b9c84955a44aac72fafa3255cd620c56b1
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-06 13:36:17 -0800

    dog.h and README files added

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains C program and C header files that illustrates the use of structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,10 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#define struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
