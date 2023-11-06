44d065ae734b87e3e14ecbb2cff5f56e9ca3abd2
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-06 13:52:41 -0800

    dog.h and README files added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef FIRST_STRUCT_H
 #define FIRST_STRUCT_H
 
+/**
+ * struct dog - creates an object templates for dogs
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * This struct create a template for object dog
+ */
+
 struct dog
 {
 	char *name;
