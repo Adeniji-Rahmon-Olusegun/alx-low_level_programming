6443c213f0986de5f97d9b6910d40b55df50b4af
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-06 13:43:46 -0800

    dog.h and README files added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,13 @@
-#ifndef STRUCT_H
-#define STRUCT_H
+#ifndef FIRST_STRUCT_H
+#define FIRST_STRUCT_H
 
 struct dog
 {
-	char *name;
+	char name[50];
+
 	float age;
-	char *owner;
+
+	char owner[50];
 };
+
 #endif
