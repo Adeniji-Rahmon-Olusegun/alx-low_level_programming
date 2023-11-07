8b633995b19867792d61cc08906be60446fed471
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-06 17:51:46 -0800

    1-init_dog.c file added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include "dog.h"
 #include <stdlib.h>
 /**
@@ -11,12 +10,6 @@
  *
  * Return: void
  */
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
