168ce9e524457de031a15dae9a1842d73cf1835f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-07 14:49:38 -0800

    5-free_dog.c file added

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory cobtaining dog
+ * template initialization
+ *
+ * @d: pointer
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
