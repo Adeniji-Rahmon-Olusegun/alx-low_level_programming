8092db8096d8cce72b639636725cb790a8563c11
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-08 10:02:16 -0800

    1-array_iterator.c file added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,5 +6,5 @@ int _putchar(char c);
 #include <stddef.h>
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
