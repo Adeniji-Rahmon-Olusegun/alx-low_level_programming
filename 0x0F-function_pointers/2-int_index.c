7c3bdb35d2d7b44d90b4c04b566671e29a478022
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-08 11:00:54 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array of integers
+ * @size: array size/len
+ * @cmp: function pointer
+ *
+ * Return: index position of integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int idx, result;
+
+	int tracker = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (idx = 0; idx < size; idx++)
+		{
+			result = (*cmp)(array[idx]);
+
+			if (result == 1)
+			{
+				return (idx);
+			}
+
+			tracker += 1;
+		}
+	}
+
+	if (tracker == size)
+	{
+		return (-1);
+	}
+
+	return (0);
+}
