21932092ba31cac0bfb5fd7b31796a8a954630c9
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-09 07:46:00 -0800

    1-print_numbers.c files added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(pn_arg, n);
 
-	for (idx = 0; idx < n, idx++)
+	for (idx = 0; idx < n; idx++)
 	{
 		printf("%d", va_arg(pn_arg, int));
 
