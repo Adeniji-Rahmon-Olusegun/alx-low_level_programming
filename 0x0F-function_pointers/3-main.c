6aebf7d69c5a24bb3da7fccd73d057ed79ca4c69
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-08 19:57:22 -0800

    3-calc.h, 3-op_functions.c, 3-get_op_func.c and 3-main.c files added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the result of arithmetic
+ * operation between two numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	int (*arithmetic)(int, int);
+
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	if (
+	    strcmp(argv[2], "+") != 0 &&
+	    strcmp(argv[2], "-") != 0 &&
+	    strcmp(argv[2], "*") != 0 &&
+	    strcmp(argv[2], "/") != 0 &&
+	    strcmp(argv[2], "%") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (
+	   (strcmp(argv[2], "/") == 0 && atoi(argv[3]) == 0) ||
+	   (strcmp(argv[2], "%") == 0 && atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	arithmetic = get_op_func(argv[2]);
+
+	if (arithmetic != NULL)
+		result = (*arithmetic)(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
