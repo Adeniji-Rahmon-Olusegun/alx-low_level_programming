ed6b1fe65d241312ede741e4d37da542f82e8258
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-16 12:34:46 -0800

    README, 0-print_list.c and lists.h files added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,25 +11,23 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *temp_var;
-	size_t counts_of_nodes;
+	size_t counts_of_nodes = 0;
 
-	temp_var = h;
-	counts_of_nodes = 0;
+	if (h == NULL)
+		return (1);
 
-	while (temp_var != NULL)
+	while (h != NULL)
 	{
-		if ((temp_var->str) == NULL)
+		if ((h->str) == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 
-		printf("[%d] %s\n", temp_var->len, temp_var->str);
+		printf("[%u] %s\n", h->len, h->str);
 
-		temp_var = temp_var->next;
+		h = h->next;
 
 		counts_of_nodes++;
 	}
-	free((void *)temp_var);
 	return (counts_of_nodes);
 }
