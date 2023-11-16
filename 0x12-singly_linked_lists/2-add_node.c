8ae6e281ed44e1f3d6595e347bb2099d12387559
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-16 15:25:51 -0800

    2-add_node.c file added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *duplicate_str;
+	size_t count_dup_str, i;
 
 	list_t *fresh_node;
 
 	duplicate_str = strdup(str);
+
+	count_dup_str = 0;
+
+	for (i = 0; duplicate_str[i] != '\0'; i++)
+		count_dup_str++;
 
 	fresh_node = (list_t *)malloc(sizeof(list_t));
 
@@ -23,6 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	fresh_node->str = duplicate_str;
+	fresh_node->len = count_dup_str;
 	fresh_node->next = *head;
 	*head = fresh_node;
 
