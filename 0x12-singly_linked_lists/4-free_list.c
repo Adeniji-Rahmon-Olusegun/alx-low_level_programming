6e4fc4a8315083491827ea770467625d54a15e40
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-16 20:55:40 -0800

    4-free_list.c file added

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,15 +11,14 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp_var;
+	list_t *temp_var, *next_node;
 
 	temp_var = head;
 
 	while (temp_var != NULL)
 	{
-		temp_var = head;
-		head = head->next;
-
+		next_node = temp_var->next;
 		free(temp_var);
+		temp_var = next_node;
 	}
 }
