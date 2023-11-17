df4be94c390d50955ec3247f69277442bcac1b1f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-16 17:33:20 -0800

    2-add_node.c file added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,17 +13,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	char *duplicate_str;
 	size_t count_dup_str, i;
-
 	list_t *fresh_node;
 
+	if (head == NULL)
+		return (NULL);
 	duplicate_str = strdup(str);
-
 	count_dup_str = 0;
 
 	for (i = 0; duplicate_str[i] != '\0'; i++)
 		count_dup_str++;
 
-	fresh_node = malloc(sizeof(list_t));
+	fresh_node = (list_t *)malloc(sizeof(list_t));
 
 	if (fresh_node == NULL)
 		return (NULL);
