3fba2287884d255f8dcb8abdb1fc0bfa6f5e70b2
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-21 07:26:13 -0800

    7-get_nodeint.c file added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	if (index == NULL)
+	if (index == 0)
 		return (head);
 
 	temp_var = head;
