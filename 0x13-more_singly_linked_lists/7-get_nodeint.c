ab536bfda97359f9a84a31f321b459dd8133c6b2
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-21 07:44:41 -0800

    7-get_nodeint.c file added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,36 @@
 #include "lists.h"
 
 /**
+ * listint_size - returns the length of a
+ * single linked list
+ *
+ * @h: head of the single linked list
+ *
+ * Return: length/size of the single-
+ * linked-list
+ */
+
+size_t listint_size(const listint_t *h)
+{
+	const listint_t *temp_var;
+	size_t size_of_list;
+
+	if (h == NULL)
+		return (0);
+
+	temp_var = h;
+	size_of_list = 0;
+
+	while (temp_var != NULL)
+	{
+		size_of_list++;
+		temp_var = temp_var->next;
+	}
+
+	return (size_of_list);
+}
+
+/**
  * get_nodeint_at_index - retrieves the nth node
  * of the single linked list
  *
@@ -21,7 +51,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 
-	node_length = listint_len(head);
+	node_length = listint_size(head);
 
 	if (index > node_length)
 		return (NULL);
