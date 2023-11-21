3e7c9349794b1d10a87b01e3bc7d6065f2a9e0c3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-20 19:45:31 -0800

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a single linked list
+ *
+ * @head: head of the list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp_var;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (temp_var != NULL)
+	{
+		temp_var = head;
+		head = head->next;
+		free(temp_var);
+	}
+
+}
