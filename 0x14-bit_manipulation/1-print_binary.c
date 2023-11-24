23415a150e4bcc69213a45b9678fae2ec78d0b85
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-23 18:33:57 -0800

    1-print_binary.c file added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 	int binary_power;
 	unsigned long int value_update, binary_base;
 
-	binary_power = 11;
+	binary_power = 12;
 	value_update = n;
 
 	if (n == 0)
