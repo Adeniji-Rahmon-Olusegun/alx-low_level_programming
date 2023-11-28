aede6f2173c33a0c8794f4cc0d62e386c51c75ca
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2023-11-28 07:06:35 -0800

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,11 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_dr, byte_to_write;
+	size_t string_length;
+	int index, file_dr, byte_to_write;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
 	file_dr = open(filename, O_WRONLY | O_TRUNC);
 
@@ -28,22 +27,24 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
-	if (text_content == NULL)
-	{
-		file_dr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	index = 0;
+	string_length = 0;
 
-		if (file_dr == -1)
+	if (text_content != NULL)
+	{
+		while (text_content[index] != '\0')
+		{
+			string_length++;
+			index++;
+		}
+	
+		byte_to_write = write(file_dr, text_content, string_length);
+
+		if (byte_to_write == -1)
 			return (-1);
-
-		return (1);
 	}
 
-	byte_to_write = write(file_dr, text_content, (sizeof(text_content) - 2));
-
-	if (byte_to_write == -1)
-	{
-		return (-1);
-	}
+	close(file_dr);
 
 	return (1);
 }
