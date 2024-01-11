7bf3cd0a3c4c070605c8d044a6e0fd17865b9afc
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-11 14:03:49 -0800

    4-free_dlistint.c file added

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ *
+ * @head: head of the list
+ *
+ * Return: None
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp_var, *del_node;
+
+	if (head->next == NULL)
+		free(head);
+
+	temp_var = head;
+
+	while (temp_var->next != NULL)
+	{
+		del_node = head;
+		temp_var = temp_var->next;
+		head = head->next;
+		head->prev = NULL;
+		free(del_node);
+	}
+	free(temp_var);
+}
