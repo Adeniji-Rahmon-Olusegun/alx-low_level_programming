7e6facefdd3e45f4b522849397f71e55afa13aa3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-11 19:10:16 -0800

    8-delete_dnodeint.c file added

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,48 @@
 #include "lists.h"
 
 /**
+ * add_dnodeint - adds a node at the beginning
+ * of a doubly linked list
+ *
+ * @head: address of the head
+ * @n: data value to be stored in new node
+ *
+ * Return: Address to the new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *fresh_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	fresh_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (fresh_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		fresh_node->n = n;
+		fresh_node->prev = NULL;
+		fresh_node->next = NULL;
+		*head = fresh_node;
+		return (fresh_node);
+	}
+
+	fresh_node->n = n;
+	fresh_node->prev = NULL;
+	fresh_node->next = NULL;
+
+	(*head)->prev = fresh_node;
+	fresh_node->next = *head;
+	*head = fresh_node;
+
+	return (fresh_node);
+}
+
+/**
  * delete_dnodeint_at_index - deletes a node at a given index
  *
  * @head: pointer to the first node
