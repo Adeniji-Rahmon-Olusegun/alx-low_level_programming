0878261170e14bd2161c172bd751ffe533b724a6
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-02 09:29:12 -0800

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,58 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: pointer to hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *temp, *next_n;
+
+	if (ht == NULL)
+		return;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		temp = ht->array[idx];
+
+		while (temp)
+		{
+			next_n = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next_n;
+		}
+
+		free(ht->array);
+		free(ht);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
