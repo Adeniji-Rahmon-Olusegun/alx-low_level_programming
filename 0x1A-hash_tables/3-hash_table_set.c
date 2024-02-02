a69f298577d9dcc47d86ea01f37dea2d677fb93e
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-02 03:28:20 -0800

    3-hash_table_set.c file added

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key to be inserted
+ * @value: value associated with key
+ *
+ * Return: 1 if success and 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int ht_idx;
+	hash_node_t *ht_nnode;
+
+	if (key == NULL)
+		return (0);
+	if (ht == NULL)
+		return (0);
+
+	ht_nnode = malloc(sizeof(hash_node_t));
+
+	if (ht_nnode == NULL)
+		return (0);
+
+	ht_idx = key_index(key, ht->size);
+	ht_nnode->key = strdup(key);
+	ht_node->key = strdup(value);
+	ht_nnode->next = NULL;
+
+	if (ht[ht_idx] == NULL)
+	{
+		ht->array[ht_idx] = ht_nnode;
+		return (1);
+	}
+
+	ht[0] = ht_nnode;
+
+	return (1);
+}
