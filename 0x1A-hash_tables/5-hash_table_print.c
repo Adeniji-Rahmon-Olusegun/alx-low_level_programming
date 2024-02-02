29a1520c3dbfd772117bd2a35c562e6dc01c9b6f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-02 08:34:13 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	int flag;
 	hash_node_t *temp;
 	unsigned long int idx;
 
@@ -16,14 +17,16 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 
 	printf("{");
+	flag = 0;
 	for (idx = 0; idx < ht->size; idx++)
 	{
 		temp = ht->array[idx];
 		while (temp != NULL)
 		{
-			printf("'%s: %s'", temp->key, temp->value);
-			if (temp->next != NULL)
+			if (flag)
 				printf(", ");
+			printf("'%s: %s'", temp->key, temp->value);
+			flag = 1;
 			temp = temp->next;
 
 		}
