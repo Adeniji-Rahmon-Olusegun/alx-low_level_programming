b370adf0e04757e59fb7d0bb1a4ce978ad891552
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-02 03:59:14 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,19 +13,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int ht_idx;
-	hash_node_t *ht_nnode;
+	hash_node_t *ht_nnode, *temp;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
-	if (ht == NULL)
-		return (0);
+
+	ht_idx = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[ht_idx];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 
 	ht_nnode = malloc(sizeof(hash_node_t));
 
 	if (ht_nnode == NULL)
 		return (0);
 
-	ht_idx = key_index((const unsigned char *)key, ht->size);
 	ht_nnode->key = strdup(key);
 	ht_nnode->key = strdup(value);
 	ht_nnode->next = NULL;
@@ -36,7 +46,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (1);
 	}
 
-	ht->array[0] = ht_nnode;
+	ht_nnode->next = ht->array[ht_idx];
+	ht->array[ht_idx] = ht_nnode;
 
 	return (1);
 }
